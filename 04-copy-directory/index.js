94ba97f3f10d4588c4660e6fa113f580da694067
Nikita Gromov <nikita.gromoff@gmail.com>
2023-05-02 18:01:44 +0300

    feat: task 4 done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const fs = require('fs');
+const path = require('path');
 
+    let curPath = path.join(__dirname, 'files');
+    let newPath = path.join(__dirname, 'files-copy');
+    
+
+async function copyDir (curPath, newPath) {
+    
+    fs.mkdir(newPath, {recursive : true}, (err, res) => {
+        if (res == undefined) console.log(`Directory already exist!`);
+        else console.log(`Directory created!`);
+
+        fs.readdir(curPath, (err, res) => {
+            res.forEach(el => {
+                 fs.createReadStream(path.join(`${curPath}`, `${el}`))
+                 .pipe(fs.createWriteStream(path.join(`${newPath}`, `${el}`)))
+            })
+            console.log(`Files copied in ${newPath}`);
+        })
+    })
+}
+
+copyDir(curPath, newPath)
