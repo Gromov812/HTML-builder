7aa4bc56315384176c52989417c36f0e1e28bada
Nikita Gromov <nikita.gromoff@gmail.com>
2023-05-02 20:17:42 +0300

    feat: task 5 done

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
+const stylesDir = path.join(__dirname, 'styles');
+const bundleDir = path.join(__dirname, 'project-dist')
+
+fs.readdir(stylesDir, (err, res) => {
+    
+    let bundle = '';
+
+    res.forEach(el => {
+        if (path.extname(el) == '.css') {
+            console.log(el);
+            let rs = fs.createReadStream(path.join(stylesDir, el), {encoding: 'utf-8'});
+            let ws = fs.createWriteStream(path.join(bundleDir, 'bundle.css'));
+            
+            rs.on('data', res => {
+                bundle += res;        
+                ws.write(bundle);
+            })
+        }
+    })
+
+    
+   
+})
