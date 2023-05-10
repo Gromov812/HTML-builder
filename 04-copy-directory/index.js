0a98c3570bf7b42b0a438ebc105596cd792529c8
Nikita Gromov <nikita.gromoff@gmail.com>
2023-05-10 11:38:11 +0300

    fix: fix updating files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,27 +1,25 @@
 const fs = require('fs');
 const path = require('path');
 
-    let curPath = path.join(__dirname, 'files');
-    let newPath = path.join(__dirname, 'files-copy');
-    
-    async function copyDir (curPath, newPath) {
-   
-    fs.rmdir(newPath, {recursive: true}, (err, res) => {
-       
-        fs.mkdir(newPath, {recursive : true}, (err, res) => {
+let curPath = path.join(__dirname, 'files');
+let newPath = path.join(__dirname, 'files-copy');
+
+async function copyDir(curPath, newPath) {
+
+    fs.rmdir(newPath, { recursive: true }, (err, res) => {
+
+        fs.mkdir(newPath, { recursive: true }, (err, res) => {
             if (res == undefined) console.log(`Directory already exist!`);
             else console.log(`Directory created!`);
-    
+
             fs.readdir(curPath, (err, res) => {
                 res.forEach(el => {
-                     fs.createReadStream(path.join(`${curPath}`, `${el}`))
-                     .pipe(fs.createWriteStream(path.join(`${newPath}`, `${el}`)))
+                    fs.createReadStream(path.join(`${curPath}`, `${el}`))
+                        .pipe(fs.createWriteStream(path.join(`${newPath}`, `${el}`)))
                 })
                 console.log(`Files copied in ${newPath}`);
             })
         })
     })
-
 }
-
 copyDir(curPath, newPath)
